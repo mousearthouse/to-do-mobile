0fb8c95387f0a014c4630eb00487931181e0fff8
PolinaAdashkevich <polinatulenka@gmail.com>
2024-09-18 22:06:43 +0300

    HELPMEPLS

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.todolist
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.todolist.databinding.ActivityMainBinding
+import java.util.UUID
 
-data class Task (val desc: String,
-                 val done: Boolean)  {
+data class Task(
+    val desc: String,
+    val done: Boolean,
+    val id: UUID,
+    val isEdited: Boolean
+) {
 }
 
 class MainActivity : ComponentActivity() {
@@ -14,23 +20,58 @@ class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
 
     val tasksList = arrayListOf<Task>()
-    val adapter = SmolAdapter()
+    val adapter = SmolAdapter(this::deleteTask, this::editTask, this::setCheck)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val recycledView = binding.tasksRecycler
-        recycledView.layoutManager = LinearLayoutManager(this) // Добавляем LayoutManager
+        recycledView.layoutManager = LinearLayoutManager(this)
         recycledView.adapter = adapter
 
+
         binding.addTask.setOnClickListener {
-            addTask("NewTask")
+            if (getText() != "") {
+                addTask(getText())
+            } else {
+                Toast.makeText(this, "Write something", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
+    private fun getText(): String {
+        return binding.writeTask.text.toString()
+    }
+
     private fun addTask(description: String) {
-        tasksList.add(Task(description, false))
-        adapter.submitList(tasksList.toList()) // Создаем неизменяемую копию списка
+        tasksList.add(Task(description, false, UUID.randomUUID(), false))
+        adapter.submitList(tasksList.toList())
+    }
+
+    private fun setCheck(position: Int) {
+        val editedCheck = tasksList[position].copy(done = !tasksList[position].done)
+        tasksList[position] = editedCheck
+        adapter.submitList(tasksList.toList())
+    }
+
+    private fun deleteTask(position: Int) {
+        tasksList.removeAt(position)
+        adapter.submitList(tasksList.toList())
+    }
+
+    private fun editTask(position: Int, text: String) {
+        val thisTask = tasksList[position]
+        if (thisTask.isEdited == false) {
+            val isEditedCheck = thisTask.copy(isEdited = true)
+            tasksList[position] = isEditedCheck
+            adapter.submitList(tasksList.toList())
+        }
+        else {
+            val isEditedCheck = thisTask.copy(desc = text, isEdited = false)
+            tasksList[position] = isEditedCheck
+            adapter.submitList(tasksList.toList())
+        }
     }
 }
--- app/src/main/java/com/example/todolist/SmolAdapter.kt
+++ app/src/main/java/com/example/todolist/SmolAdapter.kt
@@ -5,27 +5,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class SmolAdapter: ListAdapter<Task, SmolViewHolder>(DIFF_CALLBACK) {
+class SmolAdapter(
+    val deleteTask: (Int) -> Unit,
+    val editTask: (Int, String) -> Unit,
+    val setCheck: (Int) -> Unit
+): ListAdapter<Task, SmolViewHolder>(DIFF_CALLBACK) {
 
     companion object {
         val DIFF_CALLBACK: DiffUtil.ItemCallback<Task> = object : DiffUtil.ItemCallback<Task>() {
             override fun areItemsTheSame(
                 oldTask:Task, newTask:Task
             ): Boolean {
-                //Task properties may have changed if reloaded from the DB, but ID is fixed
-                return oldTask == newTask
+                return oldTask.id == newTask.id
             }
 
             override fun areContentsTheSame(
                 oldTask:Task, newTask:Task
             ): Boolean {
-                // NOTE: if you use equals, your object must properly override Object#equals()
-                // Incorrectly returning false here will result in too many animations.
                 return oldTask == newTask
             }
         }
@@ -33,30 +36,57 @@ class SmolAdapter: ListAdapter<Task, SmolViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SmolViewHolder {
 
-            // Create a new view, which defines the UI of the list item
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.task_view, parent, false)
 
-            return SmolViewHolder(view)
+            return SmolViewHolder(view, deleteTask, editTask, setCheck)
     }
 
     override fun onBindViewHolder(holder: SmolViewHolder, position: Int) {
         holder.textView.text = getItem(position).desc
         holder.doneChk.isChecked = getItem(position).done
+
+        holder.editText.setText(getItem(position).desc)
+        if (getItem(position).isEdited) {
+            holder.editText.isVisible = true
+            holder.textView.isVisible = false
+        } else {
+            holder.editText.isVisible = false
+            holder.textView.isVisible = true
+        }
     }
 }
 
+class SmolViewHolder(
+    view: View,
+    val deleteTask: (Int) -> Unit,
+    val editTask: (Int, String) -> Unit,
+    val setCheck: (Int) -> Unit
+) : RecyclerView.ViewHolder(view) {
 
-class SmolViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val textView: TextView
     val editBtn: Button
     val deleteBtn: Button
     val doneChk: CheckBox
+    val editText: EditText
 
     init {
         textView = view.findViewById(R.id.task_text)
         editBtn = view.findViewById(R.id.edit_btn)
         deleteBtn = view.findViewById(R.id.delete_btn)
         doneChk = view.findViewById(R.id.checkbox)
+        editText = view.findViewById(R.id.edit_text)
+
+        deleteBtn.setOnClickListener {
+            deleteTask(bindingAdapterPosition)
+        }
+
+        editBtn.setOnClickListener {
+            editTask(bindingAdapterPosition, editText.text.toString())
+
+        }
+
+        doneChk.setOnCheckedChangeListener { buttonView, isChecked ->  setCheck(bindingAdapterPosition)}
+
     }
 }
